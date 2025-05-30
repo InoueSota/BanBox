cd59a0f492fed2c87e4cd6969bce6701b620a3c4
InoueSota <k022g0045@g.neec.ac.jp>
2025-05-30 22:39:31 +0900

    とりあえずプロトタイプ完成

--- Assets/3_Scripts/GameManager.cs
+++ Assets/3_Scripts/GameManager.cs
@@ -6,10 +6,22 @@ public class GameManager : MonoBehaviour
     // My Component
     private InputManager inputManager;
 
+    [Header("Clear")]
+    [SerializeField] private string nextSceneName;
+    [SerializeField] private float changeSceneIntervalTime;
+    private float changeSceneIntervalTimer;
+    private bool isClear;
+
+    [Header("UI")]
+    [SerializeField] private GameObject clearTextObj;
+
     void Start()
     {
         // Set Component
         inputManager = GetComponent<InputManager>();
+
+        // Variables - Initialize
+        changeSceneIntervalTimer = changeSceneIntervalTime;
     }
 
     void Update()
@@ -17,13 +29,34 @@ public class GameManager : MonoBehaviour
         // 入力状況を取得する
         inputManager.GetAllInput();
 
+        Clear();
+
         // 完全リセット
         if (inputManager.IsTrgger(inputManager.reset)) { SceneManager.LoadScene(SceneManager.GetActiveScene().name); }
     }
-
     void LateUpdate()
     {
         // 入力状況をリセットする
         inputManager.SetIsGetInput();
+    }
+
+    void Clear()
+    {
+        if (!isClear)
+        {
+            bool noBox = true;
+
+            foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Object"))
+            {
+                if (obj.GetComponent<AllObjectManager>().GetIsActive() && obj.GetComponent<AllObjectManager>().GetObjectType() == AllObjectManager.ObjectType.BOX) { noBox = false; }
+            }
+
+            if (noBox) { clearTextObj.SetActive(true); isClear = true; }
+        }
+        else
+        {
+            changeSceneIntervalTimer -= Time.deltaTime;
+            if (changeSceneIntervalTimer <= 0f) { SceneManager.LoadScene(nextSceneName); }
+        }
     }
 }
